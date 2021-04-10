89e1a4375b78b5a85a0845d5a36a6b8f46240eb1
JGreenOS <jsciteach@gmail.com>
2021-04-10 19:33:52 -0400

    added model for burger

---
+++ models/burger.js
@@ -0,0 +1,20 @@
+//Reference the ORM to bring the functions for the database
+
+const orm = require('../config/orm.js');
+
+const burger = {
+    all(cb) {
+        orm.all('burger', (res) => cb(res));
+    },
+    create(cols, vals, cb) {
+        orm.create('burger', cols, vals, (res) => cb(res));
+    },
+    update(objColVals, condition, cb) {
+        orm.update('burger', objColVals, condition, (res) => cb(res));
+    },
+    delete(condition, cb) {
+        orm.delete('burger', condition, (res) => cb(res));
+    },
+};
+
+module.exports = burger;
