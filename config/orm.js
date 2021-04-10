ece1c532a7380bb83c69d06e9507cf80440326b7
JGreenOS <jsciteach@gmail.com>
2021-04-10 19:20:56 -0400

    orm config

--- config/orm.js
+++ config/orm.js
@@ -1,26 +1,99 @@
 //import MYsqL connection
-
-
+const { query } = require('./connection.js');
+const connection = require('./connection.js');
 
 //helper function for SQL syntax for question marks in queries
+const printQuestionMarks = (num) => {
+    const arr = [];
+    for (let i = 0; i < num; i++)
+    {
+        arr.push('?');
+    }
+    return arr.toString();
 
+};
 
 //helper function to take object key/value pairs to SQL
-
-
-        //helper function to create strings into an array
-
+const objToSQL = (ob) => {
+    const arr = [];
+        for (const key in ob) {
+            let value = ob[key];
+            //skip hidden properties, if they exist
+            if (Object.hasOwnProperty.call(ob, key)){
+                //add quotes to strings that may have spaces
+                if (typeof value === 'string' && value.indexOf(' ') >= 0){
+                    value = `'${value}`;
+                }
+                arr.push(`${key} =${value}`);
+            }
+        }
         //translate the array into a csv string
-
+return arr.toString();
+};
 
 //object for SQL statement functions
 
+const orm = {
+all(tableInput, cb) {
+    const queryString = `SELECT * FROM ${tableInput};`;
+    connection.query(queryString, (err, result) => {
+        if (err) {
+            throw err;
+        }
+        cb(result);
+    });
+},
+//create
+create (table, cols, vals, cb) {
+    let queryString = `INSERT INTO ${table}`;
+    queryString += '  (';
+    queryString += cols.toString();
+    queryString += ')  ';
+    queryString += 'VALUES (';
+    queryString += printQuestionMarks(vals.length);
+    queryString += ')  ';
 
-    //create
+    console.log(queryString);
 
+    connection.query(queryString, vals, (err, result) => {
+        if (err) {
+            throw err;
+        }
+    cb(result);
+    });
+},
     //update
+update(table, objColVals, condition, cb){
+    let queryString = `UPDATE ${table}`;
 
-    //delete
+    queryString += ' SET ';
+    queryString += objToSQL(objColVals);
+    queryString += ' WHERE ';
+    queryString += condition;
 
+    console.log(queryString);
+    connection.query(queryString, (err, results) => {
+        if (err) {
+            throw err;
+        }
+    cb(result);
+    });
+},
+  //delete
+delete(table, condition, cb) {
+let queryString = `DELETE FROM ${table}`;
+queryString += ' WHERE ';
+queryString += condition;
 
-//export the orm to the model for burgers    
+connection.query(queryString, (err, result) => {
+    if (err) {
+        throw err;
+    }
+    cb(result);
+});
+
+},
+
+};
+//export the orm to the model for burgers   
+module.exports = orm; 
