7d7d29df8ebc1d53dba2308bc16239a10b3fd837
JGreenOS <jsciteach@gmail.com>
2021-04-10 20:12:43 -0400

    server.js file complete

--- server.js
+++ server.js
@@ -8,9 +8,17 @@ const app = express();
 app.use(express.static('public'));
 
 //apps as JSON
-
+app.use(express.urlencoded( { extended: true } ));
+app.use(express.json());
 //handlebars
+const exphbs = require('express-handlebars');
 
+app.engine('handlebars', exphbs( { defaultLayout: 'main' } ));
+app.set('view engine', 'handlebars');
 //routes
-
+const routes = require('./controllers/burger_controllers');
+app.use(routes);
 //start the server
+app.listen(PORT, () =>
+console.log(`Server listening on : http://localhost:${PORT}`)
+);
