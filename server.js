bde1fc9baeba3b110a3a457626cea341658c12f7
JGreenOS <jsciteach@gmail.com>
2021-04-10 20:06:02 -0400

    adding assets folder

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 //require express
-
+const express = require('express');
 
 // env variables
-
+const PORT = process.env.PORT || 8080;
+const app = express();
 //static content for app
+app.use(express.static('public'));
 
 //apps as JSON
 
