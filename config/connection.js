2c9466ed058906c0168b99e9ea3a87799ad7df92
JGreenOS <jsciteach@gmail.com>
2021-04-25 00:01:21 -0400

    db configs

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,23 @@
 const mysql = require('mysql');
 require('dotenv').config();
 
-var connection;
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-    connection = mysql.createConnection({
-        host: 'localhost',
-        port: 3306,
-        user: 'root',
-        password: '5jkhxtu2',
-        database: 'burgers_db',
+const connection = mysql.createConnection({
+    host:process.env.HOST,
+    user:process.env.USER,
+    password:process.env.PASSWORD,
+    database:process.env.DBNAME
 });
-}
+// if (process.env.JAWSDB_URL) {
+//     connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//     connection = mysql.createConnection({
+//         host: 'localhost',
+//         port: 3306,
+//         user: 'root',
+//         password: '5jkhxtu2',
+//         database: 'burgers_db',
+// });
+// }
 
 connection.connect((err) => {
     if (err) {
